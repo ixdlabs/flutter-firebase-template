ada51b1a058b46c47a85675a5cf087976b681d39
K. D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2022-07-30 21:08:20 +0530

    Add default snapshot error widget

--- lib/utils/snapshot_utils.dart
+++ lib/utils/snapshot_utils.dart
@@ -9,13 +9,32 @@ extension SnapshotBuilder<T> on AsyncSnapshot<T> {
     Widget Function()? onLoading,
   }) {
     if (hasError) {
-      return onError == null ? Text("Error: $error") : onError(error);
+      return onError == null
+          ? DefaultSnapshotErrorWidget(error: error)
+          : onError(error);
     } else if (connectionState != ConnectionState.waiting && hasData) {
       return onData(data as T);
     }
     return onLoading == null
         ? const DefaultSnapshotLoadingWidget()
         : onLoading();
+  }
+}
+
+class DefaultSnapshotErrorWidget extends StatelessWidget {
+  final Object? error;
+
+  const DefaultSnapshotErrorWidget({Key? key, required this.error})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text("Error: $error"),
+      ),
+    );
   }
 }
 
