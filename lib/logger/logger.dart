64d5e9d1cd4aafcffc7b9317852bc26d89703007
K. D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2022-08-02 11:12:08 +0530

    Make logger a singleton

--- lib/logger/logger.dart
+++ lib/logger/logger.dart
@@ -6,45 +6,69 @@ import 'package:logger/logger.dart';
 /// A singleton logger class.
 /// Errors logs will be sent to crashlytics.
 class Log {
-  static late Logger _logger;
+  static late final Log _instance;
+  final Logger logger;
+  FirebaseCrashlytics? firebaseCrashlytics;
+  FirebaseAnalytics? firebaseAnalytics;
 
-  Log._();
+  Log._({required this.logger});
 
+  /// Initialize the logger.
+  /// This method must be called before any other method.
   static Future<void> initialize() async {
-    _logger = Logger(printer: _MainAppLogPrinter());
+    // Don't log anything below warnings in production.
+    if (kReleaseMode) Logger.level = Level.warning;
+    final logger = Logger(printer: _MainAppLogPrinter());
+    _instance = Log._(logger: logger);
   }
 
+  /// Set the firebase services to use.
+  static void setFirebaseServices(
+      {required FirebaseCrashlytics crashlytics,
+      required FirebaseAnalytics analytics}) {
+    _instance.firebaseCrashlytics = crashlytics;
+    _instance.firebaseAnalytics = analytics;
+    Log.i("Logging using firebase Crashlytics and Analytics.");
+  }
+
+  /// Logs a message at the [Level.verbose] level.
   static void v(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    _logger.v(message, error, stackTrace);
+    _instance.logger.v(message, error, stackTrace);
   }
 
+  /// Logs a message at the [Level.debug] level.
   static void d(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    _logger.d(message, error, stackTrace);
+    _instance.logger.d(message, error, stackTrace);
   }
 
+  /// Logs a message at the [Level.info] level.
   static void i(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    _logger.i(message, error, stackTrace);
+    _instance.logger.i(message, error, stackTrace);
   }
 
+  /// Logs a message at the [Level.warning] level.
   static void w(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    _logger.w(message, error, stackTrace);
+    _instance.logger.w(message, error, stackTrace);
   }
 
+  /// Logs a message at the [Level.error] level.
   static void e(dynamic message,
       [dynamic error, StackTrace? stackTrace]) async {
-    _logger.e(message, error, stackTrace);
-    await FirebaseCrashlytics.instance
-        .recordError(error, stackTrace, reason: message);
+    _instance.logger.e(message, error, stackTrace);
+    await _instance.firebaseCrashlytics
+        ?.recordError(error, stackTrace, reason: message);
   }
 
+  /// Logs a message at the [Level.wtf] level.
   static void wtf(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    _logger.wtf(message, error, stackTrace);
+    _instance.logger.wtf(message, error, stackTrace);
   }
 
+  /// Logs a analytics event.
   static void a(String name, [Map<String, Object?>? parameters]) async {
-    _logger.i("Analytics [$name] Parameters:  $parameters");
-    await FirebaseAnalytics.instance
-        .logEvent(name: name, parameters: parameters);
+    _instance.logger.i("Analytics [$name] Parameters:  $parameters");
+    await _instance.firebaseAnalytics
+        ?.logEvent(name: name, parameters: parameters);
   }
 }
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
@@ -10,42 +11,49 @@ import 'package:flutter_firebase_template/app.dart';
 import 'package:flutter_firebase_template/constants.dart';
 import 'package:flutter_firebase_template/firebase_options.dart';
 import 'package:flutter_firebase_template/logger/logger.dart';
-import 'package:logger/logger.dart';
 
 void main() async {
-  // Don't log anything below warnings in production.
-  if (kReleaseMode) Logger.level = Level.warning;
   await Log.initialize();
 
   // Initialize firebase.
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  final firebaseAuth = FirebaseAuth.instance;
+  final firebaseFirestore = FirebaseFirestore.instance;
+  final firebaseRemoteConfig = FirebaseRemoteConfig.instance;
+  final firebaseCrashlytics = FirebaseCrashlytics.instance;
+  final firebaseMessaging = FirebaseMessaging.instance;
+  final firebaseAnalytics = FirebaseAnalytics.instance;
+  Log.setFirebaseServices(
+    crashlytics: firebaseCrashlytics,
+    analytics: firebaseAnalytics,
+  );
   Log.i("Firebase app initialized.");
+
   // If variable is set, enable firebase emulators.
   if (DebugConstants.enableEmulators) {
-    await FirebaseAuth.instance.useAuthEmulator(
+    await firebaseAuth.useAuthEmulator(
         DebugConstants.emulatorHost, DebugConstants.emulatorAuthPort);
-    FirebaseFirestore.instance.useFirestoreEmulator(
+    firebaseFirestore.useFirestoreEmulator(
         DebugConstants.emulatorHost, DebugConstants.emulatorFirestorePort);
     Log.w("Running in emulator mode. Connected to emulators at "
         "${DebugConstants.emulatorHost}.");
   }
 
   // Remote Config
-  await FirebaseRemoteConfig.instance.setConfigSettings(RemoteConfigSettings(
+  await firebaseRemoteConfig.setConfigSettings(RemoteConfigSettings(
     fetchTimeout: const Duration(minutes: 1),
     // Set cache expiration to one hour in production.
     minimumFetchInterval: const Duration(minutes: kReleaseMode ? 60 : 1),
   ));
   Log.i("Remote Config initialized.");
   // Crashlytics
-  await FirebaseCrashlytics.instance
-      .setCrashlyticsCollectionEnabled(kReleaseMode);
-  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+  await firebaseCrashlytics.setCrashlyticsCollectionEnabled(kReleaseMode);
+  FlutterError.onError = firebaseCrashlytics.recordFlutterError;
   Log.i("Crashlytics initialized.");
   // FCM
-  await FirebaseMessaging.instance.requestPermission();
-  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+  await firebaseMessaging.requestPermission();
+  await firebaseMessaging.setForegroundNotificationPresentationOptions(
       alert: true, badge: true, sound: true);
   Log.i("FCM initialized.");
 
